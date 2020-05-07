bfa2fe66c6c609d1f51c4f2dbd283d8f204596d0
anne k <50986817+nefarious-onion@users.noreply.github.com>
2020-05-07 09:01:45 +0300

    enable cors

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const experienceRouter = require('./api/experience/experience.router');
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
+
+const FRONTEND_ORIGIN = "http://localhost:3000";
 
 const app = express();
+
+//allow chrome to do ajax call
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", FRONTEND_ORIGIN);
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE");
+    next();
+})
 
 //parse json bodies
 app.use(express.json());
