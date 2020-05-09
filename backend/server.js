6727a6cf64f428f02f7297b93ef327c636280ca7
veronika333 <vera3@inbox.ru>
2020-05-09 23:25:39 +0300

    port changed for heroku

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const app = express();
+const PORT = process.env.PORT || 8000
 var mongoose = require('mongoose');
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
-app.listen(8000, function(){
-    console.log('listening on port 8000');
+app.listen(PORT, function(){
+    console.log('listening on port');
 });
