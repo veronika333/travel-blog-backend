35999b034c47baa611e6af337f9cca3558820af0
Otso Lappalainen <otsolappalainen@Otsos-MacBook-Pro.local>
2020-05-14 15:20:36 +0300

    {add-comment-backend} WIP. Broken code atm.

--- api/comment/comment.router.js
+++ api/comment/comment.router.js
@@ -4,16 +4,16 @@ const Post = require('../models/Post');
 const Comment = require('../models/comment.schema');
 
 //get one comment
-router.get('/:comment_id', (req, res) => {
+router.get('/:comment_id', async (req, res) => {
     try{
-    const comments = Comment.findById(req.params.id);
+    const comments = await Comment.findById(req.params.id);
     res.json(comments);
 } catch(err){
     res.status(404).json({message:err});
 }
 });
 
-router.comment('/', (req, res) => {
+router.comment('/experience/:id/comment', async (req, res) => {
     const comment = new Comment ({
         _id: new mongoose.Types.ObjectId(),
         author: req.body.author,
@@ -24,7 +24,7 @@ router.comment('/', (req, res) => {
     
     // save the comment and catch if there is an error
     try{
-    const savedComment = comment.save();
+    const savedComment = await comment.save();
     res.status(201).json(savedComment);
     } catch(err) {
     res.status(404).json({ message: err });
