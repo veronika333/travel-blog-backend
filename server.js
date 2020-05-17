12969b113dc2024c34af15a7d2431d7452a0ef5c
veronika333 <vera3@inbox.ru>
2020-05-17 14:45:22 +0300

    npm version added to package.json

--- server.js
+++ server.js
@@ -1,35 +1,35 @@
 //import the package
-const express = require("express");
+const express = require('express')
+const PORT = process.env.PORT || 5000
 
-const experienceRouter = require('./api/experience/experience.router');
-const commentRouter = require('./api/comment/comment.router');
-const mongoose = require('mongoose');
-const PORT = process.env.PORT || 5000;
-require('dotenv/config');
 
-const FRONTEND_ORIGIN = "http://localhost:3000";
+// const experienceRouter = require('./api/experience/experience.router');
+// const commentRouter = require('./api/comment/comment.router');
+// const mongoose = require('mongoose');
 
-const app = express();
-// connect to DB
-mongoose.connect('mongodb+srv://Veronika3:Veronika3@cluster0-av0yo.mongodb.net/test?retryWrites=true&w=majority', 
-{ useNewUrlParser: true }, () => 
-    console.log('Connected to DB')
-);
+// const FRONTEND_ORIGIN = "http://localhost:3000";
 
-//allow chrome to do ajax call
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", FRONTEND_ORIGIN);
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE");
-    next();
-})
+// const app = express();
+// // connect to DB
+// mongoose.connect('mongodb+srv://Veronika3:Veronika3@cluster0-av0yo.mongodb.net/test?retryWrites=true&w=majority', 
+// { useNewUrlParser: true }, () => 
+//     console.log('Connected to DB')
+// );
 
-//parse json bodies
-app.use(express.json());
-//set experience router to use default route
-app.use('/experience', experienceRouter);
-//set comment router to use default route
-app.use('/experience/:id/comment', commentRouter);
+// //allow chrome to do ajax call
+// app.use((req, res, next) => {
+//     res.header("Access-Control-Allow-Origin", FRONTEND_ORIGIN);
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE");
+//     next();
+// })
+
+// //parse json bodies
+// app.use(express.json());
+// //set experience router to use default route
+// app.use('/experience', experienceRouter);
+// //set comment router to use default route
+// app.use('/experience/:id/comment', commentRouter);
 
 // routes
 app.get('/', (req, res) => {
