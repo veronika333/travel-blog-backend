9cca30883429b7b740b3f1f5eb7529ea91307eef
otsolap <59653348+otsolap@users.noreply.github.com>
2020-05-18 13:38:02 +0300

    {single-comment-api-endpoint} Backend side is now working as intended.

--- api/experience/experience.router.js
+++ api/experience/experience.router.js
@@ -1,27 +1,38 @@
 const express = require("express");
 const router = express.Router();
 const Post = require('../models/Post');
+const Comment = require('../models/comment.schema');
+
 
 //veronika's code get all experiences
 router.get('/', async (req, res) => {
-    try{
+    try {
         // return all the posts or can make a limit
-const posts = await Post.find();
-res.json(posts);
-    } catch(err){
-        res.json({message:err});
+        const posts = await Post.find();
+        res.json(posts);
+    } catch (err) {
+        res.json({ message: err });
     }
 });
 
 //get one experience
 router.get('/:id', async (req, res) => {
-    try{
-     const post = await Post.findById(req.params.id);
-    res.json(post);
- } catch(err){
-     res.status(404).json({message:err});
- }
- });
+    try {
+        const post = await Post.findById(req.params.id);
+        res.json(post);
+    } catch (err) {
+        res.status(404).json({ message: err });
+    }
+
+    // Experience-posts with comments.
+    // Using .populate() to get Experiences with comments.
+    Post.findOne({})
+    .populate('comments')
+    .exec((err, Comment) => {
+    console.log(Comment);
+    });
+    
+});
 
 //create a new post with the model Post and submit
 router.post('/', async (req, res) => {
@@ -33,44 +44,48 @@ router.post('/', async (req, res) => {
         location: req.body.location,
         story: req.body.story
     });
-    
+
     // save the post and catch if there is an error
-    try{
-    const savedPost = await post.save();
-    res.status(201).json(savedPost);
-    } catch(err) {
-    res.status(404).json({ message: err });
+    try {
+        const savedPost = await post.save();
+        res.status(201).json(savedPost);
+    } catch (err) {
+        res.status(404).json({ message: err });
     }
-    });
+});
 
 //delete experience
 router.delete('/:postId', async (req, res) => {
-    try{
-    const removePost = await Post.remove({_id: req.params.postId});
-    res.json(removePost);
-} catch(err){
-    //res.json({ message: err });
-    res.status(204).json({ message: err });
-}
+    try {
+        const removePost = await Post.remove({ _id: req.params.postId });
+        res.json(removePost);
+    } catch (err) {
+        //res.json({ message: err });
+        res.status(204).json({ message: err });
+    }
 });
 
 //Update a post 
 router.patch('/:postId', async (req, res) => {
-   try{
-  const updatePost = await Post.updateOne({ _id: req.params.postId }, 
-    { $set: {title: req.body.title,
-    imageUrl: req.body.imageUrl,
-    author: req.body.author,
-    location: req.body.location,
-    date: req.body.date,
-    story: req.body.story
-    }}
-    );
-  res.json(updatePost);
-} catch(err) {
-    //res.json({ message: err });
-    res.status(404).json({ message: err });
-}
+    try {
+        const updatePost = await Post.updateOne({ _id: req.params.postId },
+            {
+                $set: {
+                    title: req.body.title,
+                    imageUrl: req.body.imageUrl,
+                    author: req.body.author,
+                    location: req.body.location,
+                    date: req.body.date,
+                    story: req.body.story
+                }
+            }
+        );
+        res.json(updatePost);
+    } catch (err) {
+        //res.json({ message: err });
+        res.status(404).json({ message: err });
+    }
 })
+
 
 module.exports = router;
